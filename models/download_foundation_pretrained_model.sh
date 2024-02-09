b230bc28b229abfe1146137abce7eebb8cee5de6
Suraj Pai <bspai@bwh.harvard.edu>
2024-02-09 17:13:30 -0500

    Remove dbx and add hf pull

--- models/download_foundation_pretrained_model.sh
+++
@@ -1,2 +0,0 @@
-mkdir ssl
-wget https://zenodo.org/records/10528450/files/model_weights.torch?download=1 -O ssl/fmcib.torch
--- outputs/download_outputs.sh
+++
@@ -1,2 +0,0 @@
-wget https://www.dropbox.com/s/2ogwe0omeb50bq9/outputs.zip?dl=1 -O outputs.zip
-unzip outputs.zip
